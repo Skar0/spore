6a2d36b544521b7b1c4af0b876521b1d21d6c7c7
Skar0 <clement.tamines@gmail.com>
2021-05-16 14:41:03 +0200

    Added an Arena class to contain the BDD representation of a game arena

---
+++ arena.py
@@ -0,0 +1,21 @@
+
+
+class Arena:
+
+    def __init__(self):
+
+        self.vars = None
+        self.vars_bis = None
+        self.all_vars = None
+        self.mapping_bis = None
+        self.inv_mapping_bis = None
+
+        self.nbr_vertices = 0
+        self.nbr_digits_vertices = 0
+        self.nbr_functions = 1
+
+        self.player0_vertices = None
+        self.player1_vertices = None
+        self.edges = None
+        self.colors = None
+
