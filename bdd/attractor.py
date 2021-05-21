dbde1c852936e12ea993a7e327c0c538cc18f9ab
Skar0 <clement.tamines@gmail.com>
2021-05-21 14:25:13 +0200

    Added BDD implementation of monotone attractor

--- bdd/attractor.py
+++ bdd/attractor.py
@@ -96,3 +96,61 @@ def attractor_cudd(arena, s, player, manager):
         new_attractor = old_attractor | player0_vertices_attractor | player1_vertices_attractor
 
     return new_attractor
+
+
+def monotone_attractor(arena, s, priority, manager):
+    """
+    Computes the monotone attractor of the target set, meaning the attractor without visiting bigger priorities than
+    the one of the target set.
+    :param arena: the arena in which we compute the attractor
+    :type arena: Arena
+    :param s: the set for which we compute the attractor
+    :type s: dd.cudd.Function
+    :param priority: the priority of vertices in s
+    :type priority: int
+    :param manager: the BDD manager
+    :type manager: dd.cudd.BDD
+    :return: the computed attractor
+    :rtype: dd.cudd.Function
+    """
+
+    player = priority % 2  # the player for which we compute the attractor
+
+    old_attractor = manager.true
+    new_attractor = manager.true  # at first attractor only contains s
+
+    vertices_smaller_priority = manager.false
+    for prio, bdd in arena.priorities[0].items():
+        if prio <= priority:
+            vertices_smaller_priority = vertices_smaller_priority | bdd
+
+    # we don't want to include the old attractor in the first iteration, att normal avec retrait a la fin du target ? je crois qu'on doit considere lit avant OU le target set
+    first_iteration = True
+
+    # while a fixpoint is not reached
+    while old_attractor != new_attractor:
+
+        old_attractor = new_attractor
+
+        # BDD representing the old attractor set, using prime variables
+        old_attractor_prime = manager.let(arena.mapping_bis, old_attractor | s)
+
+        # BDD representing vertices with at least one successor in the old attractor
+        vertices_one_succ = bdd_func.and_exists(arena.edges, old_attractor_prime, arena.vars_bis)
+
+        # BDD representing vertices with at least one successor outside the previous attractor
+        # TODO are the vertices in the negation that don't exist ignored since they dont belong to arena.edges ?
+        # TODO is complementing the replaced attractor with prime vars same as replacing variables in complement of att?
+        vertices_all_succ = ~bdd_func.and_exists(arena.edges, ~old_attractor_prime, arena.vars_bis)
+
+        # if we compute the attractor for player 0
+        if not player:
+            player0_vertices_attractor = arena.player0_vertices & vertices_one_succ
+            player1_vertices_attractor = arena.player1_vertices & vertices_all_succ
+        else:
+            player0_vertices_attractor = arena.player0_vertices & vertices_all_succ
+            player1_vertices_attractor = arena.player1_vertices & vertices_one_succ
+
+        new_attractor = (old_attractor | player0_vertices_attractor | player1_vertices_attractor) & vertices_smaller_priority # check if this is where we should impose smaller priorities
+        # either that or we create a set with only the edges that are from a vertex with correct priority
+    return new_attractor
