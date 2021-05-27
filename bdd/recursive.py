e5952fc4d1ac39a0d1c77cf2dc1bf91dee306352
Skar0 <clement.tamines@gmail.com>
2021-05-27 19:25:03 +0200

    Simplifying expressions and TODO removal

--- bdd/recursive.py
+++ bdd/recursive.py
@@ -17,15 +17,14 @@ def recursive(arena, manager):
     winning_region_player1 = manager.false  # winning region of player 1
 
     # if the game is empty, return the empty regions
-    # TOD in the bdd impl the nbr of vertices in not maintained easily
-    if (arena.player0_vertices | arena.player1_vertices) == manager.false:
+    if arena.player0_vertices == manager.false and arena.player1_vertices == manager.false:
         return winning_region_player0, winning_region_player1
 
     else:
-        max_occurring_priority = max(arena.priorities[0].keys())  # get max priority occurring in g
+        max_occurring_priority = max(arena.priorities[0].keys())  # get max priority occurring in the arena
 
         # determining which player we are considering, if max_occurring_priority is odd : player 1 and else player 0
-        j = 1 if max_occurring_priority % 2 else 0
+        j = max_occurring_priority % 2
 
         opponent = 0 if j else 1  # getting the opponent of the player
 
@@ -36,7 +35,7 @@ def recursive(arena, manager):
         A = attractor(arena, U, j, manager)
 
         # The subgame G\A is composed of the vertices not in the attractor
-        G_A = arena.subarena(~A, manager) # TODO being consistent between bdd and non bdd for subarena
+        G_A = arena.subarena(~A, manager)
 
         # Recursively solving the subgame G\A
         winning_region_player0_G_A, winning_region_player1_G_A = recursive(G_A, manager)
@@ -115,28 +114,20 @@ def recursive_with_buchi(arena, manager):
     winning_region_player0 = manager.false  # winning region of player 0
     winning_region_player1 = manager.false  # winning region of player 1
 
-    remaining_arena1, partial_winning_region_player0, partial_winning_region_player1 = buchi_partial_solver(arena,
+    remaining_arena, partial_winning_region_player0, partial_winning_region_player1 = buchi_partial_solver(arena,
                                                                                                            manager.false,
                                                                                                            manager.false,
                                                                                                            manager)
 
-
-    remaining_arena = arena.subarena(~(partial_winning_region_player0 | partial_winning_region_player1), manager)
-    # TODO this is a test, is the yielded subarena in the partial solver correct or do i need to complement here
-    # ca a l'air correct
-    #assert remaining_arena1.player0_vertices == remaining_arena.player0_vertices
-    #assert remaining_arena1.player1_vertices == remaining_arena.player1_vertices
-
     # if the game is empty, return the empty regions
-    # TODO in the bdd impl the nbr of vertices in not maintained easily
-    if (remaining_arena.player0_vertices | remaining_arena.player1_vertices) == manager.false:
+    if remaining_arena.player0_vertices == manager.false and remaining_arena.player1_vertices == manager.false:
         return partial_winning_region_player0, partial_winning_region_player1
 
     else:
         max_occurring_priority = max(remaining_arena.priorities[0].keys())  # get max priority occurring in g
 
         # determining which player we are considering, if max_occurring_priority is odd : player 1 and else player 0
-        j = 1 if max_occurring_priority % 2 else 0
+        j = max_occurring_priority % 2
 
         opponent = 0 if j else 1  # getting the opponent of the player
 
@@ -147,7 +138,7 @@ def recursive_with_buchi(arena, manager):
         A = attractor(remaining_arena, U, j, manager)
 
         # The subgame G\A is composed of the vertices not in the attractor
-        G_A = remaining_arena.subarena(~A, manager) # TODO being consistent between bdd and non bdd for subarena
+        G_A = remaining_arena.subarena(~A, manager)
 
         # Recursively solving the subgame G\A
         winning_region_player0_G_A, winning_region_player1_G_A = recursive_with_buchi(G_A, manager)
@@ -211,14 +202,23 @@ def recursive_with_buchi(arena, manager):
 
     return winning_region_player0, winning_region_player1
 
+
 # Return the expression which is evaluate to True for vertices with priority less or equal than max_color
 def inf_prio_expr(bdd, max_prio, g):
+    """
+    This is Charly's implementation of finding smaller priorities.
+    """
     expr_res = bdd.false
     for curr_prio in range(0, max_prio + 1):
         expr_res = expr_res | g.priorities[0][curr_prio]
     return expr_res
 
+
 def monotone_attractor_cha(bdd, g, i, f, d):
+    """
+    This is Charly's implementation of monotone attractors.
+    """
+
     inf_col_expr = inf_prio_expr(bdd, d, g)
 
     f_1 = g.edges & bdd.let(g.mapping_bis, f)
@@ -255,34 +255,68 @@ def monotone_attractor_cha(bdd, g, i, f, d):
         attr_old = attr_new
     return attr_old
 
+
 def psolB(bdd, g):
+    """
+    This is Charly's implementation of psolB.
+    """
 
     if g.player0_vertices == bdd.false and g.player1_vertices == bdd.false:
+
         return bdd.false, bdd.false
+
     d = max(g.priorities[0].keys())
+
     for curr_p in range(0, d + 1):
+
         player = curr_p % 2
+
         x = g.priorities[0][curr_p] & (g.player0_vertices | g.player1_vertices)
+
         f_old = bdd.false
+
         while not (x == bdd.false or x == f_old):
+
             f_old = x
+
             m_attr_x = monotone_attractor_cha(bdd, g, player, x, curr_p)
+
             if (m_attr_x | x) == m_attr_x:
+
                 attr_ma = attractor(g, m_attr_x, player, bdd)
-                ind_game = g.subarena( ~attr_ma, bdd)
+
+                ind_game = g.subarena(~attr_ma, bdd)
+
                 (w_0, w_1) = psolB(bdd, ind_game)
+
                 if player == 0:
+
                     w_0 = w_0 | attr_ma
+
                 else:
+
                     w_1 = w_1 | attr_ma
+
                 return w_0, w_1
+
             else:
+
                 x = x & m_attr_x
 
     return bdd.false, bdd.false
 
 
 def ziel_with_psolver(g, bdd):
+    """
+    Solve the parity game provided in arena using a combinations of the recursive algorithm and the partial solver
+    implemented using bdds. This is Charly's implementation.
+    :param g: a game arena
+    :type g: Arena
+    :param bdd: the BDD manager
+    :type bdd: dd.cudd.BDD
+    :return: the solution of the provided parity game, that is the set of vertices won by each player
+    :rtype: (dd.cudd.Function, dd.cudd.Function)
+    """
 
     if g.player0_vertices == bdd.false and g.player1_vertices == bdd.false:
         return bdd.false, bdd.false
@@ -296,29 +330,42 @@ def ziel_with_psolver(g, bdd):
 
     p_max = max(g_bar.priorities[0].keys())  # get max priority occurring in g
 
-    # determining which player we are considering, if max_occurring_priority is odd : player 1 and else player 0
-    i = 1 if p_max % 2 else 0
-
     i = p_max % 2
+
     x = attractor(g_bar, g_bar.priorities[0][p_max], i, bdd)
+
     g_ind = g_bar.subarena(~x, bdd)
+
     (win_0, win_1) = ziel_with_psolver(g_ind, bdd)
+
     if i == 0:
+
         win_i = win_0
         win_i_bis = win_1
     else:
+
         win_i = win_1
         win_i_bis = win_0
+
     if win_i_bis == bdd.false:
+
         if i == 0:
             return z_0 | win_i | x, z_1
         else:
             return z_0, z_1 | win_i | x
+
     else:
+
         x = attractor(g_bar, win_i_bis, 1 - i, bdd)
+
         g_ind = g_bar.subarena(~x, bdd)
-        (win_0, win_1) = ziel_with_psolver( g_ind, bdd)
+
+        (win_0, win_1) = ziel_with_psolver(g_ind, bdd)
+
         if i == 0:
+
             return z_0 | win_0, z_1 | win_1 | x
+
         else:
+
             return z_0 | win_0 | x, z_1 | win_1
