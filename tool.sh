bfd96940fccec3ab1497105ae3f6ca10c3a41581
Skar0 <clement.tamines@gmail.com>
2021-05-29 00:32:48 +0200

    Modified the requirements and the launchers

--- tool.sh
+++ tool.sh
@@ -3,5 +3,5 @@
 # $1 is the command line parameter given to tool.sh; it corresponds to the .tlsf file
 ./tlsf2gpg.sh $1 > "generated_game.gpg"
 # the generated game is put in a temp file
-python regular_launcher.py "generated_game.gpg"
+python recursiveLauncherBDD.py "generated_game.gpg"
 # the game is then solved
