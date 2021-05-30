c60b86771cf4adffb44975920819844eef0c6036
Skar0 <clement.tamines@gmail.com>
2021-05-30 16:50:27 +0200

    Renamed the toolchain launcher

--- spore_LTL_toolchain.sh
+++ spore_LTL_toolchain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# $1 is the command line parameter given to spore_ltl_toolchain.sh; it corresponds to the .tlsf file
+# $1 is the command line parameter given to spore_LTL_toolchain.sh; it corresponds to the .tlsf file
 ./tlsf2gpg.sh $1 > "generated_game.gpg"
 # the generated game is put in a temp file
 python spore.py -gpg "generated_game.gpg"
